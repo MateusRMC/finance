3c64b712a1529b0571a5f9adb83b03adc052a458
Mateus Rocha <mateus@Mattbook.local>
2026-04-02 19:00:03 -0400

    progress bar is DONE

--- app/components/DashboardView.jsx
+++ app/components/DashboardView.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 export default function DashboardView({ expenses, categories }) {
   const [progress, setProgress] = useState(null);
+  const [daysLeft, setDaysLeft] = useState(null);
   const statementDate = 18;
 
   function calculateProgress() {
@@ -15,6 +16,7 @@ export default function DashboardView({ expenses, categories }) {
     const end = day >= statementDate + 1 ? new Date(now.getFullYear(), now.getMonth() + 1, statementDate + 1) : new Date(now.getFullYear(), now.getMonth(), statementDate + 1);
 
     const value = ((now - start) / (end - start)) * 100;
+    setDaysLeft(Math.ceil((end - now) / (1000 * 60 * 60 * 24)));
     return Math.min(value, 100);
   }
 
@@ -35,10 +37,13 @@ export default function DashboardView({ expenses, categories }) {
 
   return (
     <>
-      <div className="progressArea">
-        <div className="progressBar" style={{ width: `${progress ?? 0}%` }}>
-          <p style={{ color: "white", marginRight: "10px" }}>{progress == null ? "0%" : `${progress.toFixed(0)}%`}</p>
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-evenly" }}>
+        <div className="progressArea">
+          <div className="progressBar" style={{ width: `${progress ?? 0}%` }}>
+            <p style={{ color: "white", marginRight: "10px" }}>{progress == null ? "0%" : `${progress.toFixed(0)}%`}</p>
+          </div>
         </div>
+        <p style={{ color: "white" }}>{daysLeft == null ? "0 dias restantes" : `${daysLeft} dias restantes`}</p>
       </div>
 
       {dashCategories.map((c) => (
